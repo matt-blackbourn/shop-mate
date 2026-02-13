de157b83c5da8de68a930de5bf5646739d839de1
Matt Blackbourn <matt.blackie114@gmailcom>
2026-02-13 17:51:29 +1300

    add grou item button

--- app/src/Form/FoodItemGroupPlacementType.php
+++ app/src/Form/FoodItemGroupPlacementType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\FoodItem;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +13,18 @@ class FoodItemGroupPlacementType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('foodItem', EntityType::class, [
-            'class' => FoodItem::class,
-            'choices' => $options['food_items'],
-            'choice_label' => 'name',
-            'placeholder' => false,
-            'required' => true,
-            'mapped' => false,
-        ]);
+        $builder
+            ->add('foodItem', EntityType::class, [
+                'class' => FoodItem::class,
+                'choices' => $options['food_items'],
+                'choice_label' => 'name',
+                'placeholder' => false,
+                'required' => true,
+                'mapped' => false,
+            ])
+            ->add('supermarketId', HiddenType::class)
+            ->add('foodItemId', HiddenType::class)
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
